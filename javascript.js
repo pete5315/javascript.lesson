3dd48fd9de613525b2abb5cad272125598acb893
pete5315 <124735535+pete5315@users.noreply.github.com>
2023-03-16 15:48:22 -0500

    uppercase test

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
-function add7(number){
+function add7(){
+    let number = parseInt(prompt("Enter number"));
     number=number+7;
-    return number;
+    console.log(number);
+}
+
+
+function capitalize(){
+    document.getElementById("frm1").submit();
+    string=toUpperCase(slice.string(0,1))+toLowerCase(slice.string(1));
+    console.log(string);
 }
